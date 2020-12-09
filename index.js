422efbf7c0a2280909f9952fbfa5b9612db52354
februar-y <jarmah241@gmail.com>
2020-12-09 13:02:11 -0500

    commit tree 75fbd753e4b8245cdcd4be01786ea2699290f38d

--- index.js
+++ index.js
@@ -11,3 +11,21 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(string) {
+  let time = parseInt(string.split(":")[0])
+
+  if (time > 0 && time < 12) {
+    return "Good Morning"
+  }
+  else if (time > 12 && time < 17) {
+    return "Good Afternoon"
+  }
+  else {
+    return "Good Evening"
+  }
+}
+
+function displayMessage(string) {
+  document.getElementById("greeting").innerText = string;
+}
